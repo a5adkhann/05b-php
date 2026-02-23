403aaf9cb2cb2442c7b412cb226344a2bc2a68c1
fahadsiddiqui337 <fahadsiddiqui337@gmail.com>
2026-02-23 09:24:24 +0500

    ---

---
+++ arrayTypes.php
@@ -0,0 +1,21 @@
+<?php
+
+// indexed
+// $arr = ["Ali", 19, true];
+
+
+// associative
+// $arr = ["name" => "Ali", "age" => 19, "verified" => true];
+
+
+// multidimentional 
+$arr = ["name" => "Ali", "age" => 20, "hobbies"=> ["Read", "Play"]];
+
+
+
+echo $arr["hobbies"][0];
+// print_r($arr);
+// var_dump($arr);
+
+
+?>
---
+++ loopTypes.php
@@ -0,0 +1,23 @@
+<?php
+
+// For
+// for($i = 1; $i<=10; $i++){
+//     echo $i . "<br>";
+// }
+
+// While
+// $i = 1;
+// while($i<=10){
+//     echo $i . "<br>";
+//     $i++;
+// }
+
+// do-while
+$i = 1;
+do{
+    echo $i . "<br>";
+    $i++;
+}
+while($i>=10);
+
+?>
