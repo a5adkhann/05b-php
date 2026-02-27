86cab5075876fa85e77fb4fc738639357bd6ba6c
fahadsiddiqui337 <fahadsiddiqui337@gmail.com>
2026-02-27 09:19:58 +0500

    ---

---
+++ crud/create.php
@@ -0,0 +1,45 @@
+<?php
+include("../config/db_connection.php");
+
+if(isset($_POST['submitBtn'])){
+    $nameFieldValue = $_POST['nameField'];
+    $messageFieldValue = $_POST['messageField'];
+
+    $insert_query = "INSERT INTO users(name, message) VALUES('$nameFieldValue', '$messageFieldValue')";
+
+    $result = mysqli_query($connection, $insert_query);
+}
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Create</title>
+</head>
+
+<body>
+
+    <form method="POST">
+        <label for="">Name</label>
+        <input type="text" name="nameField">
+
+        <br>
+
+        <label for="">Message</label>
+        <textarea name="messageField"></textarea>
+
+        <br>
+
+        <button name="submitBtn">Submit</button>
+    </form>
+
+
+</body>
+
+</html>
---
+++ crud/read.php
@@ -0,0 +1,44 @@
+<?php
+include("../config/db_connection.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fetch</title>
+</head>
+<body>
+
+    <table border="1" cellspacing = "0" cellpadding = "5">
+        <tr>
+            <th>Name</th>
+            <th>Message</th>
+            <th>Action</th>
+        </tr>
+
+        <?php
+        $select_query = "SELECT * FROM users";
+        $result = mysqli_query($connection, $select_query);
+        // print_r($result);
+        while($display = mysqli_fetch_array($result)){
+        ?>
+        <tr>
+            <td><?php echo $display['name'] ?></td>
+            <td><?php echo $display['message'] ?></td>
+            <td>
+                <a href="">Edit</a>
+                <a href="">Delete</a>
+            </td>
+        </tr>
+        <?php
+        }
+        ?>
+
+
+    </table>
+
+
+
+</body>
+</html>
